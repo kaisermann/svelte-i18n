d8851a3387a68350d200787e408882cb66a7d753
Christian Kaisermann <christian@kaisermann.me>
2019-06-06 18:03:48 -0300

    Remove old unused dependency

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-
-
 export const capital = str => str.replace(/(^|\s)\S/, l => l.toUpperCase())
 export const title = str => str.replace(/(^|\s)\S/g, l => l.toUpperCase())
 export const upper = str => str.toLocaleUpperCase()
