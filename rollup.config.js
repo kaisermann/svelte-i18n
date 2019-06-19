f38b08b3a1096ff972f30070caada62cb63021cc
Christian Kaisermann <christian@kaisermann.me>
2019-06-19 10:41:57 -0300

    Remove umd bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,19 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import { terser } from "rollup-plugin-terser";
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
 
-import pkg from './package.json';
+import pkg from './package.json'
 
-const plugins =  [
-  resolve(),
-  commonjs(),
-  terser()
-]
+const plugins = [resolve(), commonjs(), terser()]
 
 export default [
-	// browser-friendly UMD build
-	{
-		input: 'src/index.js',
-		output: {
-			name: 'svelteI18n',
-			file: pkg.browser,
-			format: 'umd'
-    },
-    plugins
-	},
-	{
-		input: 'src/index.js',
-		external: [...Object.keys(pkg.dependencies), 'svelte/store'],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
+  {
+    input: 'src/index.js',
+    external: [...Object.keys(pkg.dependencies), 'svelte/store'],
+    outpfut: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
     ],
-    plugins
-	}
-];
+    plugins,
+  },
+]
