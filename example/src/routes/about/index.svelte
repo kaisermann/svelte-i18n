b8ce517455bd9e2050653525a2c52dbef83cff1d
Christian Kaisermann <christian@kaisermann.me>
2019-11-19 17:14:48 -0300

    refactor: 💡 move about messages to their own dir (not working)

---
+++ example/src/routes/about/index.svelte
@@ -0,0 +1,27 @@
+<script context="module">
+  // import { partial } from 'svelte-i18n'
+
+  export async function preload() {
+    // return partial({
+    //   'en-US': () => import('./_locales/en-US.json'),
+    //   'pt-BR': () => import('./_locales/pt-BR.json'),
+    //   'es-ES': () => import('./_locales/es-ES.json'),
+    // })
+  }
+</script>
+
+<script>
+  import { _ } from 'svelte-i18n'
+</script>
+
+<svelte:head>
+  <title>{$_('title.about', { default: 'About' })}</title>
+</svelte:head>
+
+<h1>{$_('about_this_site', { default: 'About this site' })}</h1>
+
+<p>
+  {$_('about_content[0]', {
+    default: "This is the 'about' page. There's not much here.",
+  })}
+</p>
