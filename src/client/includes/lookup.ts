a1dc2686542e17856cffaef9e69c76574d6d5abf
Christian Kaisermann <christian@kaisermann.me>
2019-11-20 22:20:56 -0300

    refactor: 💡 prevent null lookups from being cached

--- src/client/includes/lookup.ts
+++ src/client/includes/lookup.ts
@@ -8,6 +8,7 @@ import { getGenericLocaleFrom } from './utils'
 const lookupCache: Record<string, Record<string, string>> = {}
 
 const addToCache = (path: string, locale: string, message: string) => {
+  if (!message) return message
   if (!(locale in lookupCache)) lookupCache[locale] = {}
   if (!(path in lookupCache[locale])) lookupCache[locale][path] = message
   return message
