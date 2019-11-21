6f5e2b63f7a3acb60b5c480d03def4cef4fc9ff2
Christian Kaisermann <christian@kaisermann.me>
2019-11-20 22:21:36 -0300

    feat: 🎸 `addMessagesTo` method

--- src/client/stores/dictionary.ts
+++ src/client/stores/dictionary.ts
@@ -1,4 +1,5 @@
 import { writable, derived } from 'svelte/store'
+import merge from 'deepmerge'
 
 let dictionary: Record<string, Record<string, any>>
 
@@ -15,8 +16,23 @@ function hasLocaleDictionary(locale: string) {
   return locale in dictionary
 }
 
+function addMessagesTo(locale: string, ...partials: any[]) {
+  $dictionary.update(d => {
+    dictionary[locale] = merge.all<any>(
+      [dictionary[locale] || {}].concat(partials)
+    )
+    return d
+  })
+}
+
 const $locales = derived([$dictionary], ([$dictionary]) =>
   Object.keys($dictionary)
 )
 
-export { $dictionary, $locales, getDictionary, hasLocaleDictionary }
+export {
+  $dictionary,
+  $locales,
+  getDictionary,
+  hasLocaleDictionary,
+  addMessagesTo,
+}
