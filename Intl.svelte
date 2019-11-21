649b779a6ef84ef3ec77dca188215afdb04c5fcc
Christian Kaisermann <christian@kaisermann.me>
2019-11-20 22:23:29 -0300

    refactor: 💡 rename intl component

---
+++ Intl.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+  function updateLangAttr(lang) {
+    if (typeof window !== 'undefined') {
+      document.documentElement.setAttribute('lang', lang)
+    }
+  }
+</script>
+
+<script>
+  import { locale, locales, loading } from './dist/i18n.mjs'
+
+  $: updateLangAttr($locale)
+</script>
+
+<slot loading={$loading} locale={$locale} locales={$locales} />
