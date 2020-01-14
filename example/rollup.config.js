e889a41e639d5a5379a98368bb721b0cb9a9ac4a
Christian Kaisermann <christian@kaisermann.me>
2020-01-13 21:44:08 -0300

    Ignore annoying warning

--- example/rollup.config.js
+++ example/rollup.config.js
@@ -13,10 +13,18 @@ const mode = process.env.NODE_ENV
 const dev = mode === 'development'
 const legacy = !!process.env.SAPPER_LEGACY_BUILD
 
-const onwarn = (warning, onwarn) =>
-  (warning.code === 'CIRCULAR_DEPENDENCY' &&
-    /[/\\]@sapper[/\\]/.test(warning.message)) ||
+const onwarn = (warning, onwarn) => {
+  if (
+    (warning.code === 'CIRCULAR_DEPENDENCY' &&
+      /[/\\]@sapper[/\\]/.test(warning.message)) ||
+    warning.code === 'THIS_IS_UNDEFINED'
+  ) {
+    return
+  }
+
   onwarn(warning)
+}
+
 const dedupe = importee =>
   importee === 'svelte' || importee.startsWith('svelte/')
 
