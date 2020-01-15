eb3dbb6f743dc2ca5a12353b80abbf5efe9ab5e3
Christian Kaisermann <christian@kaisermann.me>
2020-01-15 11:50:08 -0300

    refactor: 💡 remove deepmerge and dlv dependencies
    No need for them anymore. We now flat the dicitonary partials on
    `addMessages`

--- src/client/includes/utils.ts
+++ src/client/includes/utils.ts
@@ -16,6 +16,21 @@ export function lower(str: string) {
   return str.toLocaleLowerCase()
 }
 
+// could use a reduce, but a simple for-in has less footprint
+export const flatObj = (obj: Record<string, any>, prefix = '') => {
+  const flatted: Record<string, string> = {}
+  for (const key in obj) {
+    const flatKey = prefix + key
+    // we want plainobjects
+    if (typeof obj[key] === 'object') {
+      Object.assign(flatted, flatObj(obj[key], `${flatKey}.`))
+    } else {
+      flatted[flatKey] = obj[key]
+    }
+  }
+  return flatted
+}
+
 const getFromQueryString = (queryString: string, key: string) => {
   const keyVal = queryString.split('&').find(i => i.indexOf(`${key}=`) === 0)
 
