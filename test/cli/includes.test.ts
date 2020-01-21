9f352845e14f09f7dfcce2cadc34a8c908e07970
Christian Kaisermann <christian@kaisermann.me>
2020-01-21 10:07:27 -0300

    refactor: 💡 rename getIn to deepGet

--- test/cli/includes.test.ts
+++ test/cli/includes.test.ts
@@ -1,17 +1,17 @@
-import { getIn } from '../../src/cli/includes/getIn'
+import { deepGet } from '../../src/cli/includes/deepGet'
 
 describe('deep object handling', () => {
   test('gets a deep property', () => {
     const obj = {
       a: { b: { c: { d: ['foo', 'bar'] } } },
     }
-    expect(getIn(obj, 'a.b.c.d.1')).toBe('bar')
+    expect(deepGet(obj, 'a.b.c.d.1')).toBe('bar')
   })
 
   test('returns undefined for if some property not found', () => {
     const obj = {
       a: { b: 1 },
     }
-    expect(getIn(obj, 'c.b')).toBe(undefined)
+    expect(deepGet(obj, 'c.b')).toBe(undefined)
   })
 })
