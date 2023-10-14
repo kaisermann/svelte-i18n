84bb251d3ed3c914243b6b725545f0836fc9dd64
Christian Kaisermann <christian@kaisermann.me>
2023-10-14 12:12:41 +0200

    chore: use esm jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+export default {
   testEnvironment: 'jsdom',
   collectCoverage: true,
   testMatch: ['<rootDir>/test/**/*.test.ts'],
